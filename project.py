2da45d7dcba8e30a1301dfe218f8c39da277d51d
Nick-Giampetro <113528133+Nick-Giampetro@users.noreply.github.com>
2024-04-15 22:24:47 -0400

    added another functions

--- project.py
+++ project.py
@@ -65,30 +65,75 @@ def Steepest_Decent_Loop(func) :
 
     return [xk_sd_b,ginf_sd_b,f_sd_b]
 
-def Step_Length(func, xk, pk):
+def Step_Length_Q(func, xk, pk, mu):
     k = 0
 
-    c1 = 1e-2
-    alpha = 2
+    c1 = 1e-3
+    alpha = 5
     gfunc = grad(func)
     while True:
         k += 1
-        sufDec = ((func(xk + alpha * pk)) < (func(xk) + c1 * alpha * jnp.dot(gfunc(xk), pk)))
-        if sufDec or (alpha < 1e-4):
-            nalpha = alpha
+        sufDec = ((func(xk + alpha * pk, mu)) < (func(xk, mu) + c1 * alpha * jnp.dot(gfunc(xk, mu), pk)))
+        if sufDec or (alpha < 1e-3):
             break
 
-        C = func(xk)
-        B = -np.linalg.norm(gfunc(xk))
+        C = func(xk, mu)
+        B = -np.linalg.norm(gfunc(xk, mu))
         alpha_i = xk + alpha * pk
-        A = (func(alpha_i) - B * alpha - C) / (alpha ** 2)
-        nalpha = -B / (2 * A)
+        A = (func(alpha_i, mu) - B * alpha - C) / (alpha ** 2)
+        alpha = -B / (2 * A)
 
-        # print(nalpha < 0.3*alpha)
+    return [alpha, k]
 
-        if nalpha < 0.25 * alpha:
-            nalpha = alpha * .5
-        alpha = nalpha
 
-    return [nalpha, k]
+def Quad_Penalty(func, x0, mu, tau, eta, rho):
+    gfunc = grad(func)
 
+    maxiters = 250  # maximum number of iterations
+
+    bounds = jnp.array([[-5., 0], [10., 15.]])  # optimization variable bounds
+
+    g_inf = np.linalg.norm(gfunc(x0, mu), ord=np.inf)  # starting gradient infinity norm
+    k = 0  # iteration counter
+    xk = x0  # starting point
+    fk = func(xk, mu)
+
+    # empty lists to store optimization history
+    ginf_sd_b = []  # first-order optimality
+    ginf_sd_b.append(g_inf)
+    xk_sd_b = []  # iterate history
+    xk_sd_b.append(x0)  # include the starting point
+    ncalls_sd_b = []  # number of function calls
+    f_sd_b = []  # objective history
+    f_sd_b.append(fk)
+
+    np.set_printoptions(precision=3)
+    print(f'starting point x0: {xk}, f0: {fk}')
+    print('----------------------------------')
+
+    while tau > 1e-4 or mu < 10:
+        gfunc = grad(func)
+        while g_inf >= tau and k < maxiters:
+            gk = gfunc(xk, mu)
+            pk = -gk / np.linalg.norm(gk)  # steepest-descent direction
+            sl = Step_Length_Q(func, xk, pk, mu)  # calculate step length
+            alpha = sl[0]
+            xk = xk + alpha * pk  # new iterate
+
+            fk = func(xk, mu)  # evaluate f at new iterate
+            g_inf = np.linalg.norm(gk, ord=np.inf)  # check first-order optimality (gradient)
+
+            k += 1
+            ncalls_sd_b.append(sl[1])
+            ginf_sd_b.append(g_inf)
+            xk_sd_b.append(xk)
+            f_sd_b.append(fk)
+
+            print(
+                f'iteration {k} ,  function calls: {sl[1]},  alpha: {alpha:1.7f}, xk: {xk.squeeze()}, fk: {fk.item():2.6f}, gradient norm: {g_inf:2.6f}')
+
+        mu = mu * rho
+        tau = tau * eta
+
+        print(f'mu: {mu} , tau: {tau}')
+    return (ginf_sd_b, xk_sd_b)
